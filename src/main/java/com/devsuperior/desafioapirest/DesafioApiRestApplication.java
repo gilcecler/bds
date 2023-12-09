35303a0b4febe1862989afe30801d040edb77f70
Gilcecler Carneiro <gilcecler@gmail.com>
2023-12-09 20:03:56 -0300

    start, cliente, clienteDTO, h2

---
+++ src/main/java/com/devsuperior/desafioapirest/DesafioApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafioapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiRestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/devsuperior/desafioapirest/dto/ClientDTO.java
@@ -0,0 +1,62 @@
+package com.devsuperior.desafioapirest.dto;
+
+import java.time.LocalDate;
+
+import com.devsuperior.desafioapirest.entities.Client;
+
+public class ClientDTO {
+	
+	private Long Id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private LocalDate birthDate;
+	private Integer children;
+	
+	public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+		
+		Id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.birthDate = birthDate;
+		this.children = children;
+	}
+	
+	public ClientDTO(Client entity) {
+		Id = entity.getId();
+		name = entity.getName();
+		cpf  = entity.getCpf();
+		income = entity.getIncome();
+		birthDate = entity.getBirthDate();
+		children = entity.getChildren();
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+	
+	
+
+	
+}
